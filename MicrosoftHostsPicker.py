b6266eaeaf56c667f2ac1f74b09b98124fbdb2b6
MIYAMATSU YOSHIRYU <11119269@gitlab.vmic.xyz>
2021-07-01 14:13:16 +0800

    fix comment format and simplify the import

--- MicrosoftHostsPicker.py
+++ MicrosoftHostsPicker.py
@@ -1,39 +1,43 @@
-import ping3
-fwrite=open(r"hosts",'w')
-n=3
+from ping3 import ping
+
+fwrite = open(r"hosts", 'w')
+n = 3
 Time = []
 result = []
 
+
 def pingIP(result):
     Time.clear()
-    TargetIP=fread.readline()
+    TargetIP = fread.readline()
     while TargetIP:
-        TotalTime=0.0
+        TotalTime = 0.0
         for i in range(n):
-            IPstrip=TargetIP.strip()
-            PingTime=ping3.ping(IPstrip,timeout=1,unit='ms')
+            IPstrip = TargetIP.strip()
+            PingTime = ping(IPstrip, timeout=1, unit='ms')
             if not PingTime:
-                TotalTime=5000.0*n
+                TotalTime = 5000.0*n
                 break
-            TotalTime=TotalTime+PingTime
+            TotalTime = TotalTime+PingTime
         print(IPstrip+','+str(int(TotalTime/n)))
         Time.append(int(TotalTime/n))
-        Time.sort();
-        if (int(TotalTime/n) == Time[0]): result.clear();result.append(IPstrip)
-        TargetIP=fread.readline()
+        Time.sort()
+        if (int(TotalTime/n) == Time[0]):
+            result.clear()
+            result.append(IPstrip)
+        TargetIP = fread.readline()
     fread.close()
     return
 
-'''
-OneDrive Hosts beta
-'''
-fread=open(r"./data/Microsoft_Login.txt",'r')
+
+# OneDrive Hosts beta
+
+fread = open(r"./data/Microsoft_Login.txt", 'r')
 fwrite.writelines('#OneDrive(Beta, only for China) '+'\n')
 fwrite.writelines('134.170.108.26'+' '+'onedrive.live.com'+'\n')
 fwrite.writelines('134.170.109.48'+' '+'skyapi.onedrive.live.com'+'\n')
 fwrite.writelines('\n')
 
-fread=open(r"./data/Microsoft_Login.txt",'r')
+fread = open(r"./data/Microsoft_Login.txt", 'r')
 pingIP(result)
 fwrite.writelines('#Microsoft Login '+'\n')
 fwrite.writelines(result[0]+' '+'logincdn.msauth.net'+'\n')
@@ -44,7 +48,7 @@ print(result)
 result.clear()
 fwrite.writelines('\n')
 
-fread=open(r"./data/Microsoft_Account.txt",'r')
+fread = open(r"./data/Microsoft_Account.txt", 'r')
 pingIP(result)
 fwrite.writelines('#Microsoft Account '+'\n')
 fwrite.writelines(result[0]+' '+'account.microsoft.com'+'\n')
@@ -52,19 +56,19 @@ print(result)
 result.clear()
 fwrite.writelines('\n')
 
-'''
-fread=open(r"./data/OneNote.txt",'r')
-pingIP(result)
-fwrite.writelines('#OneNote '+'\n')
-fwrite.writelines(result[0]+' '+'hierarchyapi.onenote.com'+'\n')
-fwrite.writelines(result[0]+' '+'contentsync.onenote.com'+'\n')
-fwrite.writelines(result[0]+' '+'d.docs.live.net'+'\n')
-print(result)
-result.clear()
-fwrite.writelines('\n')
-'''
 
-fread=open(r"./data/Xbox_Live_CDN_1.txt",'r')
+# fread=open(r"./data/OneNote.txt",'r')
+# pingIP(result)
+# fwrite.writelines('#OneNote '+'\n')
+# fwrite.writelines(result[0]+' '+'hierarchyapi.onenote.com'+'\n')
+# fwrite.writelines(result[0]+' '+'contentsync.onenote.com'+'\n')
+# fwrite.writelines(result[0]+' '+'d.docs.live.net'+'\n')
+# print(result)
+# result.clear()
+# fwrite.writelines('\n')
+
+
+fread = open(r"./data/Xbox_Live_CDN_1.txt", 'r')
 pingIP(result)
 fwrite.writelines('#Xbox Live CDN '+'\n')
 fwrite.writelines(result[0]+' '+'images-eds.xboxlive.com'+'\n')
@@ -73,7 +77,7 @@ fwrite.writelines(result[0]+' '+'gameclipscontent-d2009.xboxlive.com'+'\n')
 fwrite.writelines(result[0]+' '+'titlehub.xboxlive.com'+'\n')
 fwrite.writelines(result[0]+' '+'compass.xboxlive.com'+'\n')
 
-fread=open(r"./data/Xbox_Live_CDN_2.txt",'r')
+fread = open(r"./data/Xbox_Live_CDN_2.txt", 'r')
 pingIP(result)
 fwrite.writelines(result[0]+' '+'xnotify.xboxlive.com'+'\n')
 fwrite.writelines(result[0]+' '+'activityhub.xboxlive.com'+'\n')
@@ -86,7 +90,7 @@ print(result)
 result.clear()
 fwrite.writelines('\n')
 
-fread=open(r"./data/Xbox_Cloud_Sync.txt",'r')
+fread = open(r"./data/Xbox_Cloud_Sync.txt", 'r')
 pingIP(result)
 fwrite.writelines('#Xbox Cloud Sync '+'\n')
 fwrite.writelines(result[0]+' '+'titlestorage.xboxlive.com'+'\n')
@@ -94,7 +98,7 @@ print(result)
 result.clear()
 fwrite.writelines('\n')
 
-fread=open(r"./data/Office_CDN.txt",'r')
+fread = open(r"./data/Office_CDN.txt", 'r')
 pingIP(result)
 fwrite.writelines('#Office CDN '+'\n')
 fwrite.writelines(result[0]+' '+'officecdn.microsoft.com'+'\n')
@@ -102,7 +106,7 @@ print(result)
 result.clear()
 fwrite.writelines('\n')
 
-fread=open(r"./data/Microsoft_Store_Images.txt",'r')
+fread = open(r"./data/Microsoft_Store_Images.txt", 'r')
 pingIP(result)
 fwrite.writelines('#Microsoft Store Images '+'\n')
 fwrite.writelines(result[0]+' '+'store-images.s-microsoft.com'+'\n')
@@ -110,7 +114,7 @@ print(result)
 result.clear()
 fwrite.writelines('\n')
 
-fread=open(r"./data/Microsoft_Store_Pages.txt",'r')
+fread = open(r"./data/Microsoft_Store_Pages.txt", 'r')
 pingIP(result)
 fwrite.writelines('#Microsoft Store Pages '+'\n')
 fwrite.writelines(result[0]+' '+'storeedgefd.dsx.mp.microsoft.com'+'\n')
@@ -118,7 +122,7 @@ print(result)
 result.clear()
 fwrite.writelines('\n')
 
-fread=open(r"./data/Microsoft_Games_Download.txt",'r')
+fread = open(r"./data/Microsoft_Games_Download.txt", 'r')
 pingIP(result)
 fwrite.writelines('#Microsoft Games Download '+'\n')
 fwrite.writelines(result[0]+' '+'xvcf1.xboxlive.com'+'\n')
@@ -127,7 +131,7 @@ print(result)
 result.clear()
 fwrite.writelines('\n')
 
-fread=open(r"./data/Windows_Update.txt",'r')
+fread = open(r"./data/Windows_Update.txt", 'r')
 pingIP(result)
 fwrite.writelines('#Windows Update '+'\n')
 fwrite.writelines(result[0]+' '+'tlu.dl.delivery.mp.microsoft.com'+'\n')
